735504e6044d33d66ae57477be03014889d6df33
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-14 18:46:36 +0200

    Update script 2

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
-for j in $(seq 1 1); do
+for j in $(seq 1 100); do
 	git checkout -b issue/COMMIT-$j-branch
-		for i in $(seq 1 1); do
+		for i in $(seq 1 100); do
 			echo "$i" >> do_not_read_me_$j.txt
 			git add .
 			git commit -m "COMMIT-$i: $i + $j = $((i + j))"
@@ -8,4 +8,5 @@ for j in $(seq 1 1); do
 	git push --set-upstream origin issue/COMMIT-$j-branch
 	git checkout master --force
 done
+
 
