d1ced23b434e96fa97f812f0cef5726eee8b326c
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-14 14:12:21 +0200

    TEST-1: 1

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
-for i in $(seq 1 500000); do
-	echo "Next $i commit\n" >> do_not_read_me.txt
+for i in $(seq 1 50000); do
+	echo "$i\n" >> do_not_read_me_too.txt
 	git add .
 	git commit -m "TEST-$i: $i"
 done
