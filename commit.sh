2f071a7d0bb845080552d2f7b90d07ca9caafe7b
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-15 14:36:19 +0200

    RRR-1: 1 + 1 = 2

--- commit.sh
+++ commit.sh
@@ -1,11 +1,11 @@
 for j in $(seq 1 100); do
-	git checkout -b issue/COMMIT-$j-branch
+	git checkout -b issue/RRR-$j-branch
 		for i in $(seq 1 100); do
-			echo "$i" >> do_not_read_me_$j.txt
+			echo "$i" >> do_not_read_me_$j_R.txt
 			git add .
-			git commit -m "COMMIT-$i: $i + $j = $((i + j))"
+			git commit -m "RRR-$i: $i + $j = $((i + j))"
 		done
-	git push --set-upstream origin issue/COMMIT-$j-branch
+	git push --set-upstream origin issue/RRR-$j-branch
 	git checkout master --force
 done
 
