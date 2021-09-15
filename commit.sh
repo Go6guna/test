86cd4067051ab11a4f86b147e676bd9546a9820b
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-15 21:15:36 +0200

    COT-1: 1 + 1 = 2

--- commit.sh
+++ commit.sh
@@ -1,11 +1,12 @@
-for j in $(seq 1 1); do
-	git checkout -b issue/COMMIT-$j-branch
+for j in $(seq 1 1000); do
+#	git checkout -b issue/COMMIT-$j-branch
 		for i in $(seq 1 1); do
-			echo "$i" >> do_not_read_me_$j.txt
+			echo "$i" >> dao_not_read_me_$j.txt
 			git add .
-			git commit -m "COMMIT-$i: $i + $j = $((i + j))"
+			git commit -m "COT-$i: $i + $j = $((i + j))"
 		done
-	git push --set-upstream origin issue/COMMIT-$j-branch
-	git checkout master --force
+#	git push --set-upstream origin issue/COMMIT-$j-branch
+	git push
+#	git checkout master --force
 done
 
