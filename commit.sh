4c7ff2587d8db01fb5cf85bfff97e67229e52eb0
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-15 14:32:01 +0200

    RAND-1: 1 + 1 = 2

--- commit.sh
+++ commit.sh
@@ -1,10 +1,10 @@
-for j in $(seq 1 10); do
+for j in $(seq 1 100); do
 #	git checkout -b issue/COMMIT-$j-branch
 
-	for i in $(seq 1 100); do
-		echo "$i" >> do_not_read_me_$j_r.txt
+	for i in $(seq 1 10); do
+		echo "$i" >> do_not_read_me_$j_r2.txt
 		git add .
-		git commit -m "RANDOM-$i: $i + $j = $((i + j))"
+		git commit -m "RAND-$i: $i + $j = $((i + j))"
 	done
 
 #	git push --set-upstream origin issue/COMMIT-$j-branch
