022a01a621d0298808acb0a91df5d5434088090b
ServerDVCS <dvcs.connector@protonmail.com>
2021-09-15 14:29:46 +0200

    RANDOM-1: 1 + 1 = 2

--- commit.sh
+++ commit.sh
@@ -1,12 +1,15 @@
-for j in $(seq 1 100); do
-	git checkout -b issue/COMMIT-$j-branch
-		for i in $(seq 1 100); do
-			echo "$i" >> do_not_read_me_$j.txt
-			git add .
-			git commit -m "COMMIT-$i: $i + $j = $((i + j))"
-		done
-	git push --set-upstream origin issue/COMMIT-$j-branch
-	git checkout master --force
+for j in $(seq 1 10); do
+#	git checkout -b issue/COMMIT-$j-branch
+
+	for i in $(seq 1 100); do
+		echo "$i" >> do_not_read_me_$j_r.txt
+		git add .
+		git commit -m "RANDOM-$i: $i + $j = $((i + j))"
+	done
+
+#	git push --set-upstream origin issue/COMMIT-$j-branch
+	git push
+#	git checkout master --force
 done
 
 
